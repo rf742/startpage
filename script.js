6dc8476392eadd32d934bb836ea18e66834ab416
rf <56006623+rf742@users.noreply.github.com>
2021-08-12 21:37:34 -0400

    added openBSD man search

---
+++ script.js
@@ -0,0 +1,7 @@
+function bsd(){
+	var  baseurl = 'https://man.openbsd.org/'
+	var search_string = document.getElementById("openBSD").value;
+	var url = baseurl+ search_string
+	window.open(url)
+
+}
