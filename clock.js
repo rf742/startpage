a53e3410269c3b93465286877f31a1fa745a56f0
rf <56006623+rf742@users.noreply.github.com>
2022-03-27 21:36:45 -0400

    refactored clock.js

--- clock.js
+++ clock.js
@@ -1,14 +1,11 @@
 function startTime() {
 	var d = new Date();
-	var hr = d.getHours();
-	var min = d.getMinutes();
-	var sec = d.getSeconds();
-	hr = checkTime(hr);
-	min = checkTime(min);
-	sec = checkTime(sec);
+	var hr =  checkTime(d.getHours());
+	var min = checkTime(d.getMinutes());
+	var sec = checkTime(d.getSeconds());
 	document.getElementById("clock").innerHTML = hr + ":" + min + ":"+ sec;
 	var time = setTimeout(function(){ startTime() }, 500);
 }
 function checkTime(i){
-	if (i<10) { i = "0" + i;} return i;
+	return i<10 ? "0" + i : i;
 }
